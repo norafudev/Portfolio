2e4944ed5b87ee88bd62de19a7a35c60d25cd452
norafudev <blinkbyhayley@gmail.com>
2024-09-24 22:51:07 +0900

    chore: remove boilerplate code and update font to use LXGW WenKai

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        lxgw: ["LXGW Wenkai", "sans-serif"],
+      },
     },
   },
   plugins: [],
